bc0d53296c2352887473f538fb978ad9ff8a1b75
lyae <lyaesley@gmail.com>
2021-07-12 20:33:01 +0900

    Post Restful url 작성

--- callblog-api/src/main/java/org/callblog/controller/PostController.java
+++ callblog-api/src/main/java/org/callblog/controller/PostController.java
@@ -8,25 +8,45 @@ import org.callblog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/post")
+@RequestMapping("/api/board")
 public class PostController {
 
     @Autowired PostService postService;
 
-    @PostMapping(path = "/list")
-    public ResponseEntity<? extends BasicResponse> postList(@RequestBody @Valid Post post) {
+    @GetMapping(path = "/post")
+    public ResponseEntity<? extends BasicResponse> selectPostList(@RequestBody @Valid Post post) {
         // TODO: 2021-07-12 페이징 처리 
-        List<Post> postList = postService.postList();
+        List<Post> postList = postService.selectPostList();
 
         return ResponseEntity.ok().body(new ResultResponse<>(StatusEnum.OK));
     }
+
+    @GetMapping(path = "/post/{id}")
+    public ResponseEntity<? extends BasicResponse> selectPost(@PathVariable("id") Long id) {
+
+        Post resPost = postService.selectPost(id);
+
+        return ResponseEntity.ok().body(new ResultResponse<>(StatusEnum.OK, resPost));
+    }
+
+
+    @PostMapping(path = "/post")
+    public ResponseEntity<? extends BasicResponse> savePost(@RequestBody @Valid Post post) {
+        postService.savePost(post);
+
+        return ResponseEntity.ok().body(new ResultResponse<>(StatusEnum.OK));
+    }
+
+
+
+
+
+
 
 }
--- callblog-api/src/main/java/org/callblog/service/PostService.java
+++ callblog-api/src/main/java/org/callblog/service/PostService.java
@@ -1,6 +1,9 @@
 package org.callblog.service;
 
+import org.callblog.configuration.CallblogCommonException;
 import org.callblog.domain.Post;
+import org.callblog.model.response.ErrorResponse;
+import org.callblog.model.response.StatusEnum;
 import org.callblog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,8 +12,20 @@ import java.util.List;
 
 @Service
 public class PostService {
+
     @Autowired PostRepository postRepository;
-    public List<Post> postList() {
+
+    public List<Post> selectPostList() {
         return postRepository.findAll();
+    }
+
+    public void savePost(Post post) {
+        postRepository.save(post);
+    }
+
+    public Post selectPost(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> {
+            throw new CallblogCommonException(new ErrorResponse<>(StatusEnum.BAD_REQUEST, "E901", "게시글이 존재하지 않습니다."));
+        });
     }
 }
